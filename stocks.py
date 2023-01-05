7c34c1a6f0ffad7953094c448902f34b9b189a5c
Oleksandr Chako <oleksandr.chako@xyleme.com>
2023-01-05 13:30:22 +0100

    WIP

--- stocks.py
+++ stocks.py
@@ -9,16 +9,18 @@ import plotly.express as px
 app = Dash()
 
 app.layout = html.Div([
-    
-    dcc.Dropdown(["btcusd", "ethusd", "xrpusd"], id="coin-select", value="btcusd"),
+
+    dcc.Dropdown(["btcusd", "ethusd", "xrpusd"],
+                 id="coin-select", value="btcusd"),
     dcc.Dropdown(["60", "3600", "86400"], id="timeframe-select", value="60"),
     dcc.Dropdown(["20", "50", "100"], id="num-bars-select", value="20"),
-    
+
     dcc.Graph(id="candles"),
     dcc.Graph(id="indicator"),
-    
-    dcc.Interval(id="interval", interval = 2000),
+
+    dcc.Interval(id="interval", interval=2000),
 ])
+
 
 @app.callback(
     Output("candles", "figure"),
@@ -28,43 +30,43 @@ app.layout = html.Div([
     Input("timeframe-select", "value"),
     Input("num-bars-select", "value"),
 )
-
 def update_figure(n_intervals, coin_pair, timeframe, num_bars):
     url = f"https://www.bitstamp.net/api/v2/ohlc/{coin_pair}/"
-    
+
     params = {
         "step": timeframe,
         "limit": int(num_bars) + 14,
     }
-    
+
     data = requests.get(url, params).json()["data"]["ohlc"]
     data = pd.DataFrame(data)
-    
-    data.timestamp = pd.to_datetime(data.timestamp, unit = "s")
-    
+
+    data.timestamp = pd.to_datetime(data.timestamp, unit="s")
+
     data['rsi'] = ta.rsi(data.close.astype(float))
-    
+
     data = data.iloc[14:]
-    
+
     candles = go.Figure(
-        data = [
+        data=[
             go.Candlestick(
-                x = data.timestamp,
-                open = data.open,
-                high = data.high,
-                low = data.low,
-                close = data.close
+                x=data.timestamp,
+                open=data.open,
+                high=data.high,
+                low=data.low,
+                close=data.close
             )
         ]
     )
-    
+
     candles.update_layout(xaxis_rangeslider_visible=False, height=500)
-    
+
     indicator = px.line(x=data.timestamp, y=data.rsi, height=300)
-    
+
     print(data)
-    
+
     return candles, indicator
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- stocks2.py
+++ stocks2.py
@@ -3,6 +3,13 @@ from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 
+# TODO:
+# 1. Create range option only with range slider. Delete candells length
+# 2. Create html tag with current diaposone description
+# 3. Create radiobutton to switch beetween line and candles
+# 4. Create Indicator charts
+# 5. Styling the code
+
 # Clean data
 
 # we need to skip first row with link
@@ -36,9 +43,56 @@ app.layout = html.Div([
                  id="coin_pair", value="btceur"),
     dcc.Dropdown(["day", "hour"], id="timeframe", value="hour"),
 
-    dcc.Graph(id="candles"),
+    html.Div(children='Data description here'),
+    
+    html.Div([
+        dcc.Slider(0, 10, 1, value=5, marks=None, id="range_slider")
+    ], id="range_slider_container"),
 
+    dcc.Graph(id="candles"),
 ])
+
+
+@app.callback(
+    Output("range_slider_container", "children"),
+    Input("coin_pair", "value"),
+    Input("timeframe", "value")
+)
+def update_slider(coin_pair, timeframe):
+
+    # we need to chouse which data frame we'll use
+    if coin_pair == 'btceur':
+        df = btceur_1h, btceur_d
+    elif coin_pair == 'btcusd':
+        df = btcusd_1h, btcusd_d
+    elif coin_pair == 'ethbtc':
+        df = ethbtc_1h, ethbtc_d
+    else:
+        df = etheur_1h, etheur_d
+
+    # Since we have 2 timeframes for each data frame we need to chose which one we take
+    if timeframe == 'hour':
+        df = df[0]
+    else:
+        df = df[1]
+
+    filtered_df = df.loc[(df['date'] > '2021-12-31 00:00:00')
+                         & (df['date'] < '2023-01-01 00:00:00')]
+
+    return dcc.Slider(
+        min=0,
+        max=int(len(filtered_df)),
+        step=1,
+        value=0,
+        marks={
+            0: {'label': filtered_df.iloc[0].date.split(" ")[0]},
+            int(len(filtered_df))*0.25: {'label': filtered_df.iloc[int((len(filtered_df) - 1)*0.25)].date.split(" ")[0]},
+            int(len(filtered_df))*0.5: {'label': filtered_df.iloc[int((len(filtered_df) - 1)*0.5)].date.split(" ")[0]},
+            int(len(filtered_df))*0.75: {'label': filtered_df.iloc[int((len(filtered_df) - 1)*0.75)].date.split(" ")[0]},
+            int(len(filtered_df)): {'label': filtered_df.iloc[int(len(filtered_df))-1].date.split(" ")[0]}
+        },
+        tooltip={"placement": "bottom", "always_visible": True},
+        id="range_slider")
 
 
 @app.callback(
@@ -46,9 +100,12 @@ app.layout = html.Div([
     Input("candles_length", "value"),
     Input("coin_pair", "value"),
     Input("timeframe", "value"),
+    Input("range_slider", "value"),
 )
-def update_figure(candles_length, coin_pair, timeframe):
+def update_figure(candles_length, coin_pair, timeframe, range_slider):
     
+    # print(range_slider)
+
     # we need to chouse which data frame we'll use
     if coin_pair == 'btceur':
         data = btceur_1h, btceur_d
@@ -58,15 +115,16 @@ def update_figure(candles_length, coin_pair, timeframe):
         data = ethbtc_1h, ethbtc_d
     else:
         data = etheur_1h, etheur_d
-        
+
     # Since we have 2 timeframes for each data frame we need to chose which one we take
     if timeframe == 'hour':
         data = data[0]
     else:
         data = data[1]
-    
+        
+
     # Here we choose the length of data frame
-    data = data.iloc[:candles_length]
+    data = data.iloc[range_slider:range_slider+candles_length]
 
     candles = go.Figure(
         data=[
